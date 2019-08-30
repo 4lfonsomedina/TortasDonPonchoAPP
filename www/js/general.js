ffa716242c6c4712fb345cabae7b23742dc0179c
Alfonso Medina <4lfonsomedina@gmail.com>
2019-08-30 00:54:00 -0700

    calculo de subtotal al relizar orden

--- www/js/general.js
+++ www/js/general.js
@@ -15,23 +15,36 @@ $(document).ready(function() {
 	$(document).on("click",".mi_orden",function(){
 		cargar_orden();
 	})
-	//ordenar
-	//mi orden
+	//Pagina de ordenar
 	$(document).on("click",".ordenar",function(){
 		$(".contenido_div").load("ordenar.html");
 	})
-	//subir 
+	//bajar cantidad de tortas
 	$(document).on("click",".ord_menos",function(){
 		var input_orden=$(this).parent("div").next("div").find("input");
 		input_orden.val(input_orden.val()-1);
 		if(input_orden.val()<0){input_orden.val(0);}
+		calculo_sub_total();
 	})
+	//Subir cantidad de tortas
 	$(document).on("click",".ord_mas",function(){
 		var input_orden=$(this).parent("div").prev("div").find("input");
 		input_orden.val(input_orden.val()-0+1);
 		if(input_orden.val()<0){input_orden.val(0);}
+		calculo_sub_total();
 	})
+	//funcion para seleccionar todo el tecto de un input
 	$(document).on("click",".input_orden",function(){
 		$(this).select();
 	})
+	//Calcular subtotal de orden
+	function calculo_sub_total(){
+		subtot=0;
+		$(".input_orden").each(function() {
+			var cantidad = $(this).val();
+			var precio = $(this).attr("precio");
+			subtot+= cantidad*precio;
+		});
+		$("#sub_tot_ord").html(subtot);
+	}
 });
